3a7342a0efac12390cf126eed6e43f6bdda7aac3
cloudmunch1 <pradeep@cloudboxonline.com>
2013-06-07 14:27:01 +0530

    Update list.js

--- JSP/list.js
+++ JSP/list.js
@@ -1,15 +1,9 @@
 <%@ include file="../common/taglib.jsp" %>
-<META CONTENT="operator-list_operator" name="menu">
-<h3><s:text name="label.operator.list"/></h3>
+<META CONTENT="login" name="menu">
+<h3><s:text name="label.login"/></h3>
 
 <div class="notification yui3-u-1">
     <s:if test="hasActionErrors()">
-        <div class="error">
-            <s:actionerror/>
-        </div>
-        <div class="error">
-            <s:actionerror/>
-        </div>
         <div class="error">
             <s:actionerror/>
         </div>
@@ -20,77 +14,33 @@
         </div>
     </s:if>
 </div>
-<div class="yui3-u-1">
-    <s:if test='operatorList!=null'>
-        <table class="row" cellspacing="0" cellpadding="5" width="100%">
-            <tr class="title">
-                <th>
-                    <s:text name="label.name"/>
-                </th>
-                <th>
-                    <s:text name="label.registration.page.url"/>
-                </th>
-                <th>
-                    <s:text name="label.registration.url"/>
-                </th>
-                <th>
-                    <s:text name="label.login.page.url"/>
-                </th>
-                <th>
-                    <s:text name="label.login.url"/>
-                </th>
-                <th>
-                    <s:text name="label.betting.url"/>
-                </th>
-                <th>
-                    <s:text name="label.date.format"/>
-                </th>
-                <th>
-                    <s:text name="label.method.type"/>
-                </th>
-                <th>
-                    <s:text name="label.action"/>
-                </th>
-
-            </tr>
-            <s:iterator value="operatorList" var="operator">
-                <tr>
-                    <td>
-                        <s:property value="#operator.name"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.registrationPageUrl"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.registrationUrl"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.loginPageUrl"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.loginUrl"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.bettingUrl"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.dateFormat"/>
-                    </td>
-                    <td>
-                        <s:property value="#operator.metaData"/>
-                    </td>
-                    <td>
-                        <a href="editOperator?operatorId=<s:property value="#operator.id"/>">
-                            <div class="yui3-u-11-24 ui-icon ui-icon-edit edit_operator" style="margin-bottom: 5px;"
-                                 title='<s:text name="button.edit"/>'>&nbsp;</div>
-                        </a>
-
-                        <div id="delete-operator_<s:property value="#operator.id"/>"
-                             class="yui3-u-11-24 ui-icon ui-icon-delete delete_operator" style="margin-bottom: 5px;"
-                             title='<s:text name="button.delete"/>'>&nbsp;</div>
-                    </td>
-                </tr>
-            </s:iterator>
-        </table>
-    </s:if>
+<div class="form_content yui3-u-1-2">
+    <form action="login" method="post" id="login_form">
+        <div class="yui3-g row">
+            <div class="yui3-u-1-4"><label for="user_name"><s:text name="label.userName"/></label></div>
+            <div class="yui3-u-1-24">:</div>
+            <div class="yui3-u-5-8">
+                <input class="required" type="text" name="username" id="user_name"/>
+            </div>
+        </div>
+        <div class="yui3-g row">
+            <div class="yui3-u-1-4"><label for="password"><s:text name="label.password"/></label></div>
+            <div class="yui3-u-1-24">:</div>
+            
+<div class="yui3-u-5-8">
+                <input class="required" type="password" name="password" id="password"/>
+            </div>
+        </div>
+        <div class="yui3-g row">
+            <div class="yui3-u-7-24">&nbsp;</div>
+            <div class="yui3-u-5-8">
+                <input type="submit" name="submit" id="submit_button" class="button"
+                       value='<s:text name="button.login"/>'
+                       title='<s:text name="button.login"/>'>
+            </div>
+        </div>
+    </form>
 </div>
+<style type="text/css">
+</style>
+
